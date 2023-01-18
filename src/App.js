6293f1d1a71ad899d600d7eab311d0f5cde536b9
FBerthelot <florent.berthelot35@gmail.com>
2023-01-18 11:38:05 +0100

    TP2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const pokemons = [
+    "pikachu",
+    "salamèche",
+    "dracofeu"
+  ]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header className="header">
+        <h1>Formation React Avancé !</h1>
       </header>
-    </div>
+      <ul>
+        {pokemons.map((pokemon) => {
+          return <li key={pokemon}>{pokemon}</li>
+        })}
+      </ul>
+    </>
   );
 }
 
