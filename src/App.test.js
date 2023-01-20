dd2eab68c9478936c39f031889242a367664603d
FBerthelot <florent.berthelot35@gmail.com>
2023-01-20 09:35:05 +0100

    exo3

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,22 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('Pokemon App', () => {
+  it('should display pikachu', async () => {
+    render(<App />);
+
+    expect(screen.getByText(/pikachu/i)).toBeInTheDocument();
+  })
+
+  it('should display salamèche', async () => {
+    render(<App />);
+
+    expect(screen.getByText(/salamèche/i)).toBeInTheDocument();
+  })
+
+  it('should display dracofeu', async () => {
+    render(<App />);
+
+    expect(screen.getByText(/dracofeu/i)).toBeInTheDocument();
+  })
+})
